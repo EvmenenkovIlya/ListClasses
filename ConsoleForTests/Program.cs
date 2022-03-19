e19e5cf43ce55d4e9810f44491e7f23902627b65
Ilya <Evmenya@gmail.com>
2022-03-19 15:37:23 +0300

    Initial commit

---
+++ ConsoleForTests/Program.cs
@@ -0,0 +1,26 @@
+﻿using MyArrayList;
+
+ArrayList listempty = new ArrayList();
+
+for (int i = 0; i < 10; i++)
+{
+    listempty.AddInTheEnd(i);
+    listempty.Write();
+}
+Console.ReadLine();
+ArrayList listOne = new ArrayList( 12 );
+for (int i = 0; i < 10; i++)
+{
+    listOne.AddInTheEnd(i);
+    listOne.Write();
+}
+
+Console.ReadLine();
+
+ArrayList listArr = new ArrayList(new int[] { 5,4,3, 3,2,1,3,2,1});
+
+for (int i = 0; i < 6; i++)
+{
+    listArr.DeleteLast();
+    listArr.Write();
+}
---
+++ MyArrayList/ArrayList.cs
@@ -0,0 +1,87 @@
+﻿namespace MyArrayList
+{
+    public class ArrayList
+    {
+        public int Length { get; private set; }
+
+        private int[] _array;
+
+        public ArrayList()
+        {
+            _array = new int[10];
+            Length = 0;
+        }
+
+        public ArrayList(int element)
+        {
+            _array = new int[10];
+            _array[0] = element;
+            Length = 1;
+        }
+        public ArrayList(int[] arr)
+        {
+            int lengthOfArr = arr.Length;
+
+            _array = new int[ (int) (lengthOfArr * 1.5 + 1) ];
+            Length = lengthOfArr + 1;
+            for (int i = 0; i < lengthOfArr; i ++)
+            {
+                _array[i] = arr[i];
+            }
+        }
+
+        public void AddInTheEnd(int value)
+        {
+            if (Length >= _array.Length)
+            {
+                UpSize();
+            }
+
+            _array[Length] = value;
+            Length++;
+        }
+
+        public void DeleteLast()
+        {
+            if (Length <= ((int) _array.Length * 0.5))
+            {
+                DownSize();
+            }
+
+            _array[Length - 1] = 0;
+            Length--;
+        }
+
+        public void Write()
+        {
+            Console.Write($"L={Length} RL={_array.Length}   ");
+            for (int i = 0; i < Length; i++)
+            {
+                Console.Write($"{_array[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        private void UpSize()
+        {
+            int newLength = (int)(_array.Length * 1.5d + 1);
+            int[] newArray = new int[newLength];
+            for (int i = 0; i < _array.Length; i++)
+            {
+                newArray[i] = _array[i];
+            }
+            _array = newArray;
+        }
+        private void DownSize()
+        {
+            int newLength = (int)(_array.Length / 1.3d + 1);
+            int[] newArray = new int[newLength];
+            for (int i = 0; i < newLength; i++)
+            {
+                newArray[i] = _array[i];
+            }
+            _array = newArray;
+        }
+
+    }
+}
